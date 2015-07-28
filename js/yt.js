52c7d400378fd05c26ee348b77a9c578da1b3051
Eduardo Velez Santiago <eddvelez@outlook.com>
2015-07-28 06:59:06 +0000

    changes

--- js/yt.js
+++ js/yt.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
 					videoThumbimg = '<img src="' + videoimg + '" alt="No Image Available.">';
 					
 					//output = '<a href="viewer.html#' +videoId+'"><li>' + videoThumbimg + videoTitle + '<br></li><br><a>';
-					card = '<div class="card"> <div class="card-image"><a href="viewer.html#'+videoId+'">'+videoThumbimg+'</a></div><div class="card-content"><span><a href="viewer.html#'+videoId+'">'+videoTitle+'</a><div><br><center><a class="waves-effect waves-light btn blue darken-4 icon">Facebook</a><a class="waves-effect waves-light btn blue blue darken-1"><p class="icon">Twitter</p></a></div></div></div> </div>';
+					card = '<div class="card"> <div class="card-image"><a href="viewer.html#'+videoId+'">'+videoThumbimg+'</a></div><div class="card-content"><span><a href="viewer.html#'+videoId+'">'+videoTitle+'</a><div><br><center><a ><img class="icon" src="images/fb.png"></a>  <a><img class="icon" src="images/tt.png"></a></div></div></div> </div>';
 					$('#getVideos').append(card);
 				})		
 			}
