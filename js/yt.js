e4b5e1504b5bf1acbb9dcf1beb6d50d4f03a4c59
Eduardo Velez Santiago <eddvelez@outlook.com>
2015-07-28 17:59:48 +0000

    twitter url added

--- js/yt.js
+++ js/yt.js
@@ -31,9 +31,12 @@ $(document).ready(function() {
 					videoId = item.snippet.resourceId.videoId;
 					
 					videoThumbimg = '<img src="' + videoimg + '" alt="No Image Available.">';
+					youtubeurl= 'https://youtube.com/watch?v='+videoId;
+					twitterurl = 'http://twitter.com/intent/tweet?text='+videoTitle+" "+youtubeurl+"&lang=es";
+					//facebookurl;
 					
 					//output = '<a href="viewer.html#' +videoId+'"><li>' + videoThumbimg + videoTitle + '<br></li><br><a>';
-					card = '<div class="card"> <div class="card-image"><a href="viewer.html#'+videoId+'">'+videoThumbimg+'</a></div><div class="card-content"><span><a href="viewer.html#'+videoId+'">'+videoTitle+'</a><div><br><b>Compartir: </b><a href="#" ><img class="icon" src="images/fb.png" height=30></a>  <a href="#"><img class="icon" src="images/tt.png" height=30></a></div></div></div> </div>';
+					card = '<div class="card"> <div class="card-image"><a href="viewer.html#'+videoId+'">'+videoThumbimg+'</a></div><div class="card-content"><span><a href="viewer.html#'+videoId+'">'+videoTitle+'</a><div><br><b>Compartir: </b><a href="#" ><img class="icon" src="images/fb.png" height=30></a>  <a href="'+twitterurl+'"><img class="icon" src="images/tt.png" height=30></a></div></div></div> </div>';
 					$('#getVideos').append(card);
 				})		
 			}
