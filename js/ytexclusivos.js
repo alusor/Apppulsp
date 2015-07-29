fb3a04f99cc560350c141505c2fb086c41552577
Eduardo Velez Santiago <eddvelez@outlook.com>
2015-07-29 02:15:54 +0000

    Finished App

--- js/ytexclusivos.js
+++ js/ytexclusivos.js
@@ -29,11 +29,17 @@ $(document).ready(function() {
 					videoimg = item.snippet.thumbnails.high.url; 
 					videoTitle =  item.snippet.title;
 					videoId = item.snippet.resourceId.videoId;
-					
+					videoTitleWhiot  = videoTitle.replace("#","-");
 					videoThumbimg = '<img src="' + videoimg + '" alt="No Image Available.">';
+					youtubeurl= 'https://youtube.com/watch?v='+videoId;
+					twitterurl = 'http://twitter.com/intent/tweet?text='+videoTitle+" "+youtubeurl+"- Via el Pulso"+"&lang=es";
+					facebookurl = "http://www.facebook.com/dialog/feed?app_id=559107320883310&link="+youtubeurl+"&name="+videoTitleWhiot+"&picture="+videoimg+"&redirect_uri=https://facebook.com/";
 					
+					cardtitle= '<a href="viewer.html#'+videoId+'">'+videoTitle+'</a>';
+					cardButtons= '<div><br><b> </b><a href="'+facebookurl+'" target="_blank"><img class="icon" src="images/fb.png" height=30></a>  <a href="'+twitterurl+'" target="_blank"><img class="icon" src="images/tt.png" height=30></a></div>';
+					tableContent = '<table><tbody><tr><td>'+cardtitle+'</td><td>'+cardButtons+'</td></tr></tbody></table>';
 					//output = '<a href="viewer.html#' +videoId+'"><li>' + videoThumbimg + videoTitle + '<br></li><br><a>';
-					card = '<div class="card"> <div class="card-image"><a href="viewer.html#'+videoId+'">'+videoThumbimg+'</a></div><div class="card-content"><span class="card-title black-text"><a href="viewer.html#'+videoId+'">'+videoTitle+'</a></div></div> </div>';
+					card = '<div class="card"> <div class="card-image"><a href="viewer.html#'+videoId+'">'+videoThumbimg+'</a></div><div class="card-content"><span>'+tableContent+'</div></div> </div>';
 					$('#getVideos').append(card);
 				})		
 			}
